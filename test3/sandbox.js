e0ba44ef6d1eeac34410abbf831c15e3c7eece02
Md. Shahadat Hossain Sourov <75617620+shahadat15-14400@users.noreply.github.com>
2022-04-04 16:48:28 +0600

    Add files via upload

---
+++ test3/sandbox.js
@@ -0,0 +1,156 @@
+// for loops
+
+// for(let i = 0; i < 5; i++){
+//   console.log('in loop: ', i);
+// }
+
+//console.log('loop finished');
+//const names = ['shaun', 'mario', 'luigi'];
+
+/*for(let i = 0; i < names.length; i++){
+  // console.log(names[i]);
+  let html = `<div>${names[i]}</div>`;
+  console.log(html);
+}*/
+
+// while loops
+//const names = ['shaun', 'mario', 'luigi'];
+//let i = 0;
+
+// while(i < 5){
+//   console.log('loop: ', i);
+//   i++;
+// }
+
+// while(i < names.length){
+//   console.log(names[i]);
+//   i++;
+// }
+
+/*let i = 3;
+
+do{
+  console.log('loop: ', i);
+  i++;
+}while(i < 5) */
+
+
+// if statements
+// const age = 25;
+
+// if(age > 20){
+//   console.log('you are over 20 years old');
+// }
+
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+// if(ninjas.length > 3){
+//   console.log("that's a lot of ninjas!");
+// }
+
+/*const password = 'p@ssword';
+
+if(password.length >= 8){
+  console.log('that password is long enough');
+}*/
+
+// else if statements
+/*const password = 'p@ssword123456';
+
+if(password.length >= 12){
+  console.log('that password is mighty strong');
+} else if(password.length >= 8){
+  console.log('that password is long enough');
+} else {
+  console.log('password should be at least 8 characters long');
+}*/
+
+/*const password = 'p@ssword1234';
+
+if(password.length >= 12 && password.includes('@')){
+  console.log('that password is mighty strong');
+} else if(password.length >= 8 || password.includes('@') password.length >5){
+  console.log('that password is long enough');
+} else {
+  console.log('password should be at least 8 characters long');
+}*/
+
+
+// Logical NOT
+/*const user = false;
+
+if(!user){
+  // do something
+  console.log('you must be logged in to continue');
+}
+
+console.log(!true);
+console.log(!false);*/
+
+// break & continue
+//const scores = [50, 25, 0, 30, 100, 20, 10];
+
+/*for(let i = 0; i < scores.length; i++){
+
+  if(scores[i] === 0){
+    continue;
+  }
+
+  console.log('your score:', scores[i]);
+
+  if(scores[i] === 100){
+    console.log('congrats, you got the top score!');
+    break;
+  }
+
+}
+*/
+
+// switch statements
+//const grade = 'D';
+
+//switch(grade){
+  //case 'A':
+    //console.log('you got an A!');
+    //break;
+  //case 'B':
+   // console.log('you got a B!');
+   // break;
+  //case 'C':
+    //console.log('you got a C!');
+    //break;
+  //case 'D':
+    //console.log('you got a D!');
+    //break;
+  //case 'E':
+    //console.log('you got an E!');
+   // break;
+  //default:
+    //console.log('not a valid grade');
+//}
+
+// variables & block scope
+/*let age = 30;
+
+if(true){
+
+  // age = 40;
+  let age = 40;
+  let name = 'shaun';
+  console.log('inside 1st code block:', age, name);
+
+  if(true){
+
+    let age = 50;
+    console.log('inside 2nd code block:', age, name);
+
+  }
+
+}
+
+console.log('outside code block:', age, name);
+*/
+
+
+
+
